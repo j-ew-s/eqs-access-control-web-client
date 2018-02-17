4af6c0bc161151d45337fe60e85ff98ac2a0bd10
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-17 11:03:20 -0200

    Adding role form and list components

---
+++ EQS.AccessControl.Web/src/app/components/role/role-form/role-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-role-form',
+  templateUrl: './role-form.component.html',
+  styleUrls: ['./role-form.component.css']
+})
+export class RoleFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- EQS.AccessControl.Web/src/app/components/role/role.module.ts
+++ EQS.AccessControl.Web/src/app/components/role/role.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RoleComponent } from './role.component';
+import { RoleListComponent } from './role-list/role-list.component';
+import { RoleFormComponent } from './role-form/role-form.component';
 
 
 @NgModule({
   declarations: [
-    RoleComponent
+    RoleListComponent,
+    RoleFormComponent
   ],
   imports: [
     BrowserModule
