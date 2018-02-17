f6ff4ea86bb23fd3a994f509d2a05efdea48f78b
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-17 09:33:17 -0200

    Adding routing to app

---
+++ EQS.AccessControl.Web/src/app/app.routing.ts
@@ -0,0 +1,11 @@
+import {Router, RouterModule} from '@angular/router'
+
+import { LoginComponent }    from './components/login/login.component'
+import { HomeComponent }     from './components/home/home.component'
+
+
+export const routing = RouterModule.forRoot([
+                                                {path : 'login', component: LoginComponent },
+                                                {path : 'home', component: HomeComponent },
+                                                {path : '**',   component: HomeComponent}
+                                            ]);
