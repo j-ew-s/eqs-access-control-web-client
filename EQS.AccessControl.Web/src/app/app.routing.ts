6ebaa22b38e48c13b40c8c860d17b50e52339320
j-ew-s <gabriel.scavassa@outlook.com>
2018-02-17 11:06:11 -0200

    cleaning code

--- EQS.AccessControl.Web/src/app/app.routing.ts
+++ EQS.AccessControl.Web/src/app/app.routing.ts
@@ -7,7 +7,6 @@ import { LoginComponent }    from './components/login/login.component'
 import { HomeComponent }     from './components/home/home.component'
 import { UserListComponent } from './components/user/user-list/user-list.component';
 
-
 export const routing = RouterModule.forRoot([
                                                 {path : 'login', component: LoginComponent },
                                                 {path : 'home', component: HomeComponent },
